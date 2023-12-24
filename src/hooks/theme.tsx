a263a04ad68bba56ac2df0db7358d0a9582dbfc8
abruno36 <abruno36@gmail.com>
2023-12-24 11:39:56 -0300

    Versão final do projeto - 24121139

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -34,7 +34,7 @@ const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 const ThemeProvider: React.FC<Props> = ({ children }) => {
     const [theme, setTheme] = useState<ITheme>(() => {
-        const themeSaved = localStorage.getItem('@minha-carteira:theme');
+        const themeSaved = localStorage.getItem('@finance-dashboard:theme');
 
         if(themeSaved) {
             return JSON.parse(themeSaved);
@@ -46,10 +46,10 @@ const ThemeProvider: React.FC<Props> = ({ children }) => {
     const toggleTheme = () => {
         if(theme.title === 'dark'){
             setTheme(light);
-            localStorage.setItem('@minha-carteira:theme', JSON.stringify(light));
+            localStorage.setItem('@finance-dashboard:theme', JSON.stringify(light));
         }else{
             setTheme(dark);
-            localStorage.setItem('@minha-carteira:theme', JSON.stringify(dark));
+            localStorage.setItem('@finance-dashboard:theme', JSON.stringify(dark));
         }
     };
 
